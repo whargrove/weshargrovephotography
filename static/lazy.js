c09e1f79dd86564448c3dea2bd05a5dcb165b6b7
Wesley Hargrove <wesleyhargrove@gmail.com>
2019-04-06 18:54:48 -0700

    Implement lazy loading of images

---
+++ static/lazy.js
@@ -0,0 +1,36 @@
+if ('IntersectionObserver' in window &&
+  'IntersectionObserverEntry' in window &&
+  'intersectionRatio' in window.IntersectionObserverEntry.prototype &&
+  !('isIntersecting' in IntersectionObserverEntry.prototype)) {
+
+  Object.defineProperty(window.IntersectionObserverEntry.prototype, 'isIntersecting', {
+    get: function () {
+      return this.intersectionRatio > 0
+    }
+  })
+}
+
+let observer = new IntersectionObserver( onChange, {
+  threshold: [ 0 ],
+});
+
+function onChange( changes ) {
+  changes.forEach( change => {
+    if ( change.isIntersecting ) {
+      change.target.classList.add( 'visible' );
+      change.target.src = change.target.dataset.src;
+      observer.unobserve( change.target );
+    }
+  });
+}
+
+const imgElements = [ ...document.querySelectorAll( '.lazy-load' ) ];
+const createObserver = function () {
+  imgElements.forEach( img => {
+    observer.observe( img );
+  });
+};
+
+window.addEventListener("load", () => {
+    createObserver();
+}, false);
